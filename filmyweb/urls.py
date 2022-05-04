ec65881d6489865717d180935357b1d65c1da561
Adrian Wójcik <adrian.wojcik00@gmail.com>
2022-05-04 14:19:25 +0200

    After section:4 lesson:23

--- filmyweb/urls.py
+++ filmyweb/urls.py
@@ -1,8 +1,11 @@
 from django.contrib import admin
 from django.urls import path
 
-from filmyweb.views import wszystkie_filmy
+from filmyweb.views import wszystkie_filmy, nowy_film, edytuj_film, usun_film
 
 urlpatterns = [
-    path('wszystkie/', wszystkie_filmy)
+    path('wszystkie/', wszystkie_filmy, name="wszystkie"),
+    path('nowy/', nowy_film, name="nowy"),
+    path('edytuj/<int:id>/', edytuj_film, name="edytuj_film"),
+    path('usun/<int:id>/', usun_film, name="usun")
 ]
